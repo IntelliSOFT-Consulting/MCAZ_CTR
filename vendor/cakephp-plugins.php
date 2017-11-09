1424d01a07478888d11b340c24d53d72e3f6adcf
Eddie <eddieokemwa@gmail.com>
2017-11-09 13:58:58 +0300

    Adding Vendor

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,13 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'Acl' => $baseDir . '/vendor/cakephp/acl/',
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'BootstrapUI' => $baseDir . '/vendor/friendsofcake/bootstrap-ui/',
+        'Crud' => $baseDir . '/vendor/friendsofcake/crud/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Josegonzalez/Upload' => $baseDir . '/vendor/josegonzalez/cakephp-upload/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/'
+    ]
+];
